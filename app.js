fb2688d9a68b390aab9839e8eaa4a66f01b9968f
AnaAlb1 <l181130368@gamadero.tecnm.mx>
2022-09-12 08:50:05 -0500

    comentarios en app.js

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
+// Biblioteca de 3ros para manejar errores http
 var createError = require('http-errors');
+
+//El framework express minimalista
 var express = require('express');
+
+//Biblioteca del nucleo de node que sirve para administrar rutas
 var path = require('path');
+
+//Biblioteca externa que sirve para administrar cookies
 var cookieParser = require('cookie-parser');
+
+//Biblioteca que registra en consola solicitudes del cliente
 var logger = require('morgan');
 
+//Definicion de rutas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Crando un instancia de express
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// view engine setup (configura el motor de plantillas)
+//1.Establecer donde estaran las plantillas
+//(Vistas --> viws) 
+//app.set("<nombre de la var>"), <valor>)
+app.set('views', path.join(__dirname, 'views')); //identificar en que SO se ejecuta para poder crear la ruta adecuada
+
+//Establezco que motor precargado usare
 app.set('view engine', 'hbs');
 
+//Establezco middleware
 app.use(logger('dev'));
+
+//Middleware para parsear a json la peticion 
 app.use(express.json());
+
+//Decodifica la URL
 app.use(express.urlencoded({ extended: false }));
+
+//Parsear cookies
 app.use(cookieParser());
+
+//Servidor de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Registro rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
