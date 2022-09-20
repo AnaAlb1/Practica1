e0ea1f1ac6d54d843f9dd8eb36524d4ea5aac9b9
AnaAlb1 <l181130368@gamadero.tecnm.mx>
2022-09-19 23:05:44 -0500

    Actualizado Se cambiaron de lugar las carpetas

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', author: 'Analleli Albarrán 🙋‍♀️️' });
 });
 
 module.exports = router;
