f07079a3bfe8f4425d5e829ce55d4c275c9f6729
AnaAlb1 <l181130368@gamadero.tecnm.mx>
2022-11-27 18:16:35 -0800

    S04A03 🧪 Validacion

--- server/domains/project/projectRouter.js
+++ server/domains/project/projectRouter.js
@@ -1,21 +1,17 @@
-// Importando el Router de Express
 import { Router } from 'express';
-// Importando el controlador
+
 import projectController from './projectController';
-// Creando una isntancia del enrutador
+
+import ValidateFactory from '../../services/validateFactory';
+
+import projectValidator from './projectValidator';
+
 const router = new Router();
 
-// Enrutamos
-// GET "/project"
-// GET "/project/list"
 router.get(['/', '/list'], projectController.list);
 
-// Enrutamos
-// GET "/project/add"
 router.get(['/add', '/create'], projectController.showAddProjectForm);
 
-// POST "/project/add"
-router.post(['/add', '/create'], projectController.addProject);
+router.post(['/add', '/create'], ValidateFactory({ schema: projectValidator.projectSchema, getObject: projectValidator.getProject, }), projectController.addProject);
 
-// Exporto este tramo de ruta
 export default router;
